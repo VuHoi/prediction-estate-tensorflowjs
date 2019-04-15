c36a7693f9faf08b2175046608f2f9fadd30068a
hoideptrai123 <toilahoi007@gmail.com>
2019-04-16 00:17:28 +0700

    initial

---
+++ knn-tf/index.js
@@ -0,0 +1,42 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+
+function knn(features, labels, predictionPoint, k) {
+	const {mean, variance } = tf.moments(features,0);
+	const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(.5))
+
+	return features
+		.sub(mean)
+		.div(variance.pow(.5))
+		.sub(scaledPrediction)
+		.pow(2)
+		.sum(1)
+		.pow(.5)
+		.expandDims(1)
+		.concat(labels,1)
+		.unstack()
+		.sort((a,b)=>{
+			return a.get(0) > b.get(0) ? 1 : -1;
+		})
+		.slice(0,k)
+		.reduce((acc,tensor)=>{
+  			return acc + tensor.get(1);
+		},0)/k;
+}
+
+
+let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
+	shuffle: true,
+	splitTest: 10,
+	dataColumns: ['lat','long', 'sqft_lot', 'sqft_living'],
+	labelColumns: ['price']
+});
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+testFeatures.forEach((testPoint, index)=>{
+	const result = knn(features,labels,tf.tensor(testPoint),10);
+	const error = (testLabels[index][0]- result) / testLabels[index][0];
+	console.log(`Guess: ${result} Actual: ${testLabels[index][0]} Error: ${error * 100 }%`);
+});
